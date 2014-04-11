46af9ded1838486159fa983846920b05459e5a18
Nnoduka Eruchalu <nceruchalu@gmail.com>
2014-04-10 21:20:24 -0700

    updated heavy rotation algorithm to account for most recent song play

--- noddymix/apps/audio/views.py
+++ noddymix/apps/audio/views.py
@@ -612,9 +612,17 @@ def new_releases(request):
 
 def heavy_rotation(request):
     """
-    Description: Heavy rotation songs: i.e. top played songs.
-                 If two songs are tied for play count, then the newer song gets
-                 priority
+    Description: Heavy rotation songs: i.e. top played songs over the last few
+                 days.
+                 The algorithm is somewhat based on the ranking performed by
+                 Hacker News where each song played in the last week is scored
+                 Score = (P)/(T+2)^G
+                 where,
+                   P = number of song plays of an item
+                   T = time since last song play
+                   G = Gravity, which will be set to 1.8 just like Hacker News
+                 Reference: http://amix.dk/blog/post/19574
+                 
                  A mobile page will be redirected to the homepage
     
     Arguments:   - request: HttpRequest object
@@ -623,16 +631,40 @@ def heavy_rotation(request):
     Author:      Nnoduka Eruchalu
     """
     if request.is_ajax():
-        # heavily rotated songs are those with most plays in the last few days
-        start_date = datetime.datetime.now() - datetime.timedelta(
-            days=settings.HEAVY_ROTATION_DAYS)
+        # want to use same value of now() everywhere
+        now = datetime.datetime.now() 
         
-        # get all songs played over the last week, and sort by DESC play count
-        top_songplays = SongPlay.objects.filter(date_added__gte=start_date)
-        top_songplays_songs = [songplay.song for songplay in top_songplays]
-        top_songs_cntr = Counter(top_songplays_songs).most_common(
-            settings.SONGS_PER_PAGE)
-        songs_list = [cnt[0] for cnt in top_songs_cntr]
+        # heavily rotated songs are those with top scores in the last few days
+        start_date = now - datetime.timedelta(days=settings.HEAVY_ROTATION_DAYS)
+    
+        # get all songs played over the last week, with their play counts
+        songplays = SongPlay.objects.filter(date_added__gte=start_date)
+                
+        # populate dictionary where keys are Song objects and values are 
+        # 2-element lists of play count and hours since last play
+        songs_playcnt_playtime = {}
+        for songplay in songplays:
+            song = songplay.song
+            # if song not already in dictionary then this is most recent play
+            # time and first acknowledged play.
+            if song not in songs_playcnt_playtime:
+                # remember we actually want time since last play in hours
+                time_since_play = now - songplay.date_added
+                time_since_play = time_since_play.seconds/3600.0
+                songs_playcnt_playtime[song] = [1, time_since_play]
+            
+            # else song is already in dictionary, so account for one extra play
+            else:
+                songs_playcnt_playtime[song][0] += 1
+        
+        # update dictionary values to be song play scores
+        for k, v in songs_playcnt_playtime.items():
+            songs_playcnt_playtime[k] = v[0] / ((v[1] + 2.0)**1.8) 
+        
+        # get sorted list of songs, sorting by DESC scores.
+        songs_list = sorted(songs_playcnt_playtime, 
+                            key=songs_playcnt_playtime.get, 
+                            reverse=True)[:settings.SONGS_PER_PAGE]
         
         # finally return JSON-formatted representations of these songs
         return songs_helper(request, songs_list)
